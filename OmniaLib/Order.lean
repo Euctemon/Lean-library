0dbe4883db0763d119608ad69377c4f377ad3432
Euctemon <dave@vodickovi.cz>
2024-12-15 23:49:04 +0100

    Definition of order homomorphism

--- OmniaLib/Order.lean
+++ OmniaLib/Order.lean
@@ -1,8 +1,11 @@
 namespace Order
 
-universe u
+-- some implicit type variables
+variable {α : Type u}
+variable {β : Type v}
 
 -- building blocks for more complex relations
+
 class reflexive (α : Type u) (r : α → α → Prop) : Prop where
 refl : ∀ (a : α), r a a
 
@@ -23,7 +26,7 @@ total : ∀ (a b : α), r a b ∨ r b a
 class preorder (α : Type u) (r : α → α → Prop)
   extends reflexive α r, transitive α r : Prop
 
-class equivalence  (α : Type u) (r : α → α → Prop)
+class equivalence (α : Type u) (r : α → α → Prop)
   extends preorder α r, symmetric α r : Prop
 
 class partial_order (α : Type u) (r : α → α → Prop)
@@ -34,25 +37,60 @@ class linear_order (α : Type u) (r : α → α → Prop)
 
 
 -- strict part of relation
-def strict_of {α : Type u} (r : α → α → Prop) := fun a b => r a b ∧ ¬ a = b
-postfix:max "°" => strict_of
+def strict (r : α → α → Prop) (a b : α) : Prop := r a b ∧ ¬ a = b
 
-theorem foo [p : partial_order α r] (a b : α) (h : r° a b) : ¬ r b a := by
-intro rab
-unfold strict_of at h
-have heq : a = b := p.antisymm a b h.left rab
-exact h.right heq
+theorem strict_not_symm [pr : partial_order α r] : ∀ (a b : α), strict r a b → ¬ r b a := by
+intro a b h rba
+have : a = b := pr.antisymm a b h.left rba
+exact h.right this
 
-theorem strict_trans [p : partial_order α r] (a b c : α) (h1 : r° a b) (h2 : r° b c) : r° a c := by
-unfold strict_of
-unfold strict_of at h1 h2
+theorem strict_trans [pr : partial_order α r] : ∀ (a b c : α), strict r a b → strict r b c → strict r a c := by
+intro a b c rab rbc
 apply And.intro
 case left =>
-  exact p.trans a b c h1.left h2.left
+  exact pr.trans a b c rab.left rbc.left
 case right =>
-  intro hac
-  have hbc : ¬ c = b := hac ▸ h1.right
-  apply hbc
-  have rcb := hac ▸ h1.left
-  have := p.antisymm b c h2.left rcb
+  intro eq_ac
+  apply eq_ac ▸ rab.right
+  have : r c b := eq_ac ▸ rab.left
+  have : b = c := pr.antisymm b c rbc.left this
   exact Eq.symm this
+
+
+
+
+def hom_order (r : α → α → Prop) (s : β → β → Prop) (f : α → β) : Prop := ∀ (a b : α), r a b → s (f a) (f b)
+
+def hom_rel (r : α → α → Prop) (s : β → β → Prop) (f g : α → β) : Prop := ∀ (a b : α), r a b → s (f a) (g b)
+
+theorem hom_rel.refl (f : α → β) : hom_order r s f → hom_rel r s f f := by
+intro h
+unfold hom_rel
+intro a b rab
+exact h a b rab
+
+theorem hom_rel.antisymm (f g : α → β) [pr : reflexive α r] [ps : antisymmetric β s]
+ : hom_rel r s f g → hom_rel r s g f → f = g := by
+intro h1 h2
+apply funext
+intro a
+have sfg : s (f a) (g a) := h1 a a (pr.refl a)
+have sgf : s (g a) (f a) := h2 a a (pr.refl a)
+exact ps.antisymm (f a) (g a) sfg sgf
+
+theorem hom_rel.trans (f g j : α → β) [pr : reflexive α r] [ps : transitive β s]
+  : hom_order r s j → hom_rel r s f g → hom_rel r s g j → hom_rel r s f j := by
+intro h1 h2 h3
+intro a b rab
+have sfg := h2 a a (pr.refl a)
+have sgj := h3 a a (pr.refl a)
+have foo := ps.trans (f a) (g a) (j a) sfg sgj
+have doo := h1 a b rab
+exact ps.trans (f a) (j a) (j b) foo doo
+
+
+
+/-
+https://en.wikipedia.org/wiki/Galois_connection
+
+-/
--- OmniaLib/Set.lean
+++ OmniaLib/Set.lean
@@ -19,13 +19,15 @@ exact h x
 -- Subset relation induces partial order
 def subset (A B : set α) : Prop := ∀ (e : α), e ∈ A → e ∈ B
 infix:50 " ⊆ " => subset
+infix:50 " ⊂ " => Order.strict subset
 
 theorem set_refl (A : set α) : A ⊆ A := by
 unfold subset
 intro e
 exact fun h => h
 
-instance : Order.reflexive (set α) subset where
+/-
+instance : @Order.reflexive (set α) subset where
 refl := set_refl
 
 theorem set_antisymm (A B : set α) : A ⊆ B → B ⊆ A → A = B := by
@@ -35,7 +37,7 @@ intro e
 unfold subset at ha hb
 exact Iff.intro (ha e) (hb e)
 
-instance : Order.antisymmetric (set α) subset where
+instance : @Order.antisymmetric (set α) subset where
 antisymm := set_antisymm
 
 theorem set_trans (A B C : set α) : A ⊆ B → B ⊆ C → A ⊆ C := by
@@ -46,12 +48,12 @@ unfold subset at hab hbc
 have : e ∈ B := hab e he
 exact hbc e this
 
-instance : Order.transitive (set α) subset where
+instance : @Order.transitive (set α) subset where
 trans := set_trans
 
-instance : Order.preorder (set α) subset := Order.preorder.mk
-instance p : Order.partial_order (set α) subset := Order.partial_order.mk
+instance : @Order.preorder (set α) subset := Order.preorder.mk
+instance : @Order.partial_order (set α) subset := Order.partial_order.mk
 
-
-theorem foo (α : Type u) (A B C : set α) (h1 : subset° A B) (h2 : subset° B C) : subset° A C := by
-exact @Order.strict_trans (set α) subset p A B C h1 h2
+theorem foo (α : Type u) (A B C : set α) (h1 : A ⊂ B) (h2 : B ⊂ C) : A ⊂ C := by
+exact Order.strict_trans h1 h2
+-/
